d209158d30ff7fcff7aecf669a223914a73cd3bf
Seine Michael <seinemichael223@gmail.com>
2024-12-30 11:57:34 +0800

    Login system implemented

--- app/includes/login-inc.php
+++ app/includes/login-inc.php
@@ -1,7 +1,6 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
     $username = $_POST["username"];
     $pwd = $_POST["pwd"];
 
@@ -10,32 +9,44 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         require_once 'login_model-inc.php';
         require_once 'login_contr-inc.php';
 
-        // Error Handlers~
         $errors = [];
 
-        if (is_input_empty($username, $pwd, $email)) {
+        if (is_input_empty($username, $pwd)) {
             $errors["empty_input"] = "Fill in all fields!";
+        }
+
+        $result = get_user($pdo, $username);
+
+        if (is_username_wrong($result)) {
+            $errors["login_incorrect"] = "Incorrect login info";
+        }
+
+        if (!is_username_wrong($result) && is_password_wrong($pwd, $result["pwd"])) {
+            $errors["login_incorrect"] = "Incorrect login info!";
         }
 
         require_once 'config_session-inc.php';
 
         if ($errors) {
-            $_SESSION["errors_signup"] = $errors;
+            $_SESSION["errors_login"] = $errors;
 
-            $signupData = [
-                "username" => $username,
-                "email" => $email
-            ];
-            $_SESSION["signup_data"] = $signupData;
-
-            header("Location: ../index.php");
-            die();
+            header("Location: ../Reg_Login/login.php");
+            exit(); // Always use exit after header()
         }
 
+        session_regenerate_id(true); // Regenerate session ID securely
+
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+        $_SESSION["last_regeneration"] = time();
+
+        header("Location: ../Homepage/homepage.html?login=success");
+        exit(); // Always use exit after header()
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
 } else {
-    header("Location: ../index.php");
-    die();
+    header("Location: ../Reg_Login/login.php");
+    exit();
 }
+
--- app/index.php
+++ app/index.php
@@ -1,6 +1,8 @@
 <?php
+ob_start();
 require_once 'includes/config_session-inc.php';
 require_once 'includes/signup_view-inc.php';
+require_once 'includes/login_view-inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +20,10 @@ require_once 'includes/signup_view-inc.php';
             <input type="password" name="pwd" placeholder="Password">
             <button>Login</button>
         </form>
+
+        <?php
+        check_login_errors();
+        ?>
 
         <h3>Signup</h3>
         <form action="includes/signup-inc.php" method="post">
