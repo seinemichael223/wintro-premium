a0b9442d1616e8b00423c86eec4183ff869cdefb
Seine Michael <seinemichael223@gmail.com>
2024-12-31 13:17:25 +0800

    load dummy data2

--- app/dummydata.php
+++ app/dummydata.php
@@ -6,8 +6,8 @@ require_once 'includes/config_session-inc.php';
 
 // Dummy Data Here:
 set_user($pdo, 'alice', 'alice', 'alice@example.com');
-set_user($pdo, 'alice', 'bob', 'bob@example.com');
-set_user($pdo, 'alice', 'charlie', 'charlie@example.com');
+set_user($pdo, 'bob', 'bob', 'bob@example.com');
+set_user($pdo, 'charlie', 'charlie', 'charlie@example.com');
 
 header("Location: Homepage/homepage.php");
 
