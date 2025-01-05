db7d27d372d15c131e4b16a902960cd8e82a4a07
wholeGrainCat <joanneee1123@gmail.com>
2025-01-05 21:51:23 +0800

    finally SUCCESS version: database

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -99,13 +99,13 @@ CREATE TABLE IF NOT EXISTS transaction_details (
     FOREIGN KEY (tid) REFERENCES transactions(tid) ON DELETE CASCADE
 );
 
-INSERT INTO users (full_name, username, pwd, email, phone_number, date_created) VALUES 
-('Alice Wong', 'aliceRabbit21', '$2y$12$nlEDNGNL384lafKuiryfI.KwsQ0JH.d0k./K/nGvlbw3ZOiwyV5Yy', 'alice@gmail.com', '012-222-5555', '2025-01-05 07:36:35', '1'),
-('John Doe', 'john_doe', 'password123', 'john.doe@example.com', '0123456789', NULL),
-('Jane Smith', 'jane_smith', 'securepwd456', 'jane.smith@example.com', '0112233445'),
-('Michael Brown', 'michael_b', 'hashedpwd789', 'michael.brown@example.com', '0133344556'),
-('Susan White', 'susan_white', 'adminpass321', 'susan.white@example.com', '0125566778'),
-('Emma Green', 'emma_g', 'userpwd654', 'emma.green@example.com', '0146677889');
+INSERT INTO users (full_name, username, pwd, email, phone_number, is_admin) VALUES 
+('Alice Wong', 'aliceRabbit21', '$2y$12$nlEDNGNL384lafKuiryfI.KwsQ0JH.d0k./K/nGvlbw3ZOiwyV5Yy', 'alice@gmail.com', '012-222-5555', '1'),
+('John Doe', 'john_doe', 'password123', 'john.doe@example.com', '0123456789', FALSE),
+('Jane Smith', 'jane_smith', 'securepwd456', 'jane.smith@example.com', '0112233445', FALSE),
+('Michael Brown', 'michael_b', 'hashedpwd789', 'michael.brown@example.com', '0133344556', FALSE),
+('Susan White', 'susan_white', 'adminpass321', 'susan.white@example.com', '0125566778', FALSE),
+('Emma Green', 'emma_g', 'userpwd654', 'emma.green@example.com', '0146677889', FALSE);
 
 INSERT INTO category (category_name) VALUES
 ('Awards and Trophies'),
