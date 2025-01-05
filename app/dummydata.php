7f42a7dd7d04f44d0671ed86d8795ebf26abd2be
Seine Michael <seinemichael223@gmail.com>
2025-01-05 11:53:04 +0800

    fixed dummydata

--- app/dummydata.php
+++ app/dummydata.php
@@ -5,9 +5,7 @@ require_once 'includes/signup_contr-inc.php';
 require_once 'includes/config_session-inc.php';
 
 // Dummy Data Here:
-set_user($pdo, 'alice', 'alice', 'alice@example.com');
-set_user($pdo, 'bob', 'bob', 'bob@example.com');
-set_user($pdo, 'charlie', 'charlie', 'charlie@example.com');
+set_user($pdo, 'Alice Wong', 'aliceRabbit21', '012-222-5555', 'Abc@123', 'alice@gmail.com');
 
 header("Location: Homepage/homepage.php");
 
